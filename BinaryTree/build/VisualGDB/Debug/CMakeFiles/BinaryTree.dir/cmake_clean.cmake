dc14999ef6b1384525753fadbc6f358c206c2410
Ivan Marzan <87321166+MarzanIvan@users.noreply.github.com>
2023-04-04 22:20:34 +0300

    VS C++ project was created

---
+++ BinaryTree/build/VisualGDB/Debug/CMakeFiles/BinaryTree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BinaryTree.exe"
+  "BinaryTree.exe.manifest"
+  "BinaryTree.pdb"
+  "CMakeFiles/BinaryTree.dir/BinaryTree.cpp.obj"
+  "CMakeFiles/BinaryTree.dir/BinaryTree.cpp.obj.d"
+  "libBinaryTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BinaryTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
