02ed31e4471a5419695de78a6a1fe5e27894a5d6
Ivan Marzan <87321166+MarzanIvan@users.noreply.github.com>
2023-04-08 13:40:04 +0300

    BinaryNode struct was created

--- BinaryTree/BinaryTree.hpp
+++ BinaryTree/BinaryTree.hpp
@@ -1,5 +1,24 @@
 #pragma once
 
+template <class Ttype>
+	struct BinaryNode {
+		Ttype Data;
+		BinaryNode<Ttype>* Parent;
+		BinaryNode<Ttype>* LeftNode;
+		BinaryNode<Ttype>* RightNode;
+		
+		BinaryNode(	Ttype& Data, 
+					BinaryNode<Ttype>* Parent = nullptr, 
+					BinaryNode<Ttype>* LeftNode = nullptr, 
+					BinaryNode<Ttype>* RightNode = nullptr
+				  ) {
+			this->Data = Data;
+			this->Parent = Parent;
+			this->LeftNode = LeftNode;
+			this->RightNode = RightNode;
+		}
+	};
+
 template<class Ttype>
 	class BinaryTree {
 		
